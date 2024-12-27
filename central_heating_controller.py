0cd6cf20bdd73ae4bc4d66b725a632f1e1746775
Wojciech Gajda <wggajda@gmail.com>
2024-12-27 19:27:09 +0100

    fix: utf8 encoding

--- central_heating_controller.py
+++ central_heating_controller.py
@@ -26,7 +26,7 @@ class CentralHeatingController:
             class RequestHandler(BaseHTTPRequestHandler):
                 def do_GET(self):
                     self.send_response(200)
-                    self.send_header('Content-type', '')
+                    self.send_header('Content-type', 'text/plain; charset=UTF-8')
                     self.end_headers()
                     status = self.server.controller.status_memento.get_status()
                     self.wfile.write(status.encode('utf-8'))
